13c08983bd92040c851393378dcb0c77e58780dd
yaboiteedoh <cook.brandon624@gmail.com>
2025-03-01 16:19:24 -0500

    removed accidental push of constructed database

--- database/pstats/__init__.py
+++
@@ -1,8 +0,0 @@
-
-def pstats_table(testing=False):
-	from ._0_0 import pstats_table
-
-	return pstats_table(testing)
-
-
-###############################################################################
--- database/teams/__init__.py
+++
@@ -1,8 +0,0 @@
-
-def teams_table(testing=False):
-	from ._0_0 import teams_table
-
-	return teams_table(testing)
-
-
-###############################################################################
