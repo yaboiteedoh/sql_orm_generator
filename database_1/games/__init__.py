32727f84e3de44d94ebd08eb391c1caf01d8c9d3
yaboiteedoh <cook.brandon624@gmail.com>
2025-03-01 17:59:30 -0500

    removed accidental constructed db push

--- database_1/games/__init__.py
+++
@@ -1,8 +0,0 @@
-
-def games_table(testing=False):
-	from ._0_0 import games_table
-
-	return games_table(testing)
-
-
-###############################################################################
--- database_1/players/__init__.py
+++
@@ -1,8 +0,0 @@
-
-def players_table(testing=False):
-	from ._0_0 import players_table
-
-	return players_table(testing)
-
-
-###############################################################################
